0259dbdd47651245b21b386be99d5f7cca4c8548
Özkan Yanikbas <oezkanyanikbas@gmail.com>
2021-12-20 18:52:52 +0100

    Add Animation to Navigation

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,5 +1,8 @@
 <template>
-  <nav class="bg-black absolute top-0 left-0 right-0 z-50">
+  <nav
+    data-aos="fade-down"
+    class="bg-black absolute top-0 left-0 right-0 z-50"
+  >
     <div
       class="py-4 container flex items-center justify-between"
     >
