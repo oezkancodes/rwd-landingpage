1bd91a8f759cc7beca6f760ac9665e3793624e21
Özkan Yanikbas <oezkanyanikbas@gmail.com>
2021-12-20 22:54:26 +0100

    Add white Button version

--- components/Button.vue
+++ components/Button.vue
@@ -1,7 +1,23 @@
 <template>
   <button
-    class="rounded-l-full rounded-r-full py-2 lg:py-3 px-6 lg:px-8 text-white bg-red-600 uppercase"
+    class="rounded-l-full rounded-r-full py-2 lg:py-3 px-6 lg:px-8 uppercase"
+    :class="
+      white
+        ? 'text-black bg-white'
+        : 'text-white bg-red-600'
+    "
   >
     <slot name="default" />
   </button>
 </template>
+
+<script>
+  export default {
+    props: {
+      white: {
+        type: Boolean,
+        default: false,
+      },
+    },
+  };
+</script>
