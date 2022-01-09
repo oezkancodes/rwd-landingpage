3972767994c95f717d0f8aa7102a05cca2c04ea4
Özkan Yanikbas <oezkanyanikbas@gmail.com>
2022-01-09 14:05:38 +0100

    Improve Feature Section with Content & Animations

--- components/FeatureSection.vue
+++ components/FeatureSection.vue
@@ -1,48 +1,78 @@
 <template>
-  <section class="py-12 lg:py-24 container text-center">
-    <!-- Title -->
-    <h2 data-aos="fade-up" class="mb-8">
-      Professionelle Überwachung
-    </h2>
+  <kinesis-container class="parent">
+    <section class="py-12 lg:py-24 container text-center">
+      <!-- Title -->
+      <h2 data-aos="fade-up" class="mb-8">
+        Professionelle Überwachung
+      </h2>
 
-    <!-- Text -->
-    <p data-aos="fade-up" class="font-bold">
-      Unsere Überwachungskameras ermöglichen Ihnen noch
-      bisher unbekannte Einblicke und Erlebnisse.
-    </p>
-
-    <!-- Image -->
-    <img
-      data-aos="fade-up"
-      class="my-6 w-full max-w-2xl mx-auto"
-      src="/images/nest-doorbell.png"
-    />
-
-    <!-- Features -->
-    <div
-      class="grid grid-cols-3 gap-y-12 lg:gap-x-6 max-w-5xl mx-auto"
-    >
-      <div
-        v-for="(item, i) in items"
-        :key="item.label"
+      <!-- Text -->
+      <p
         data-aos="fade-up"
-        :data-aos-delay="100 * i"
-        class="col-span-3 lg:col-span-1 flex flex-col items-center space-y-4"
+        class="font-bold max-w-4xl mx-auto"
       >
-        <img class="w-16 h-16" :src="item.src" />
-        <p
-          class="font-black text-center"
-          v-text="item.label"
+        Lernen Sie von den Profis. Sogar in diesem Moment
+        überwachen wir Sie mit Trackern, Cookies und
+        vielleicht mit Ihrer Webcam.
+      </p>
+
+      <div data-aos="fade-up">
+        <kinesis-element
+          class="child relative z-50"
+          type="depth"
+          :strength="30"
+        >
+          <!-- Image -->
+          <img
+            class="my-16 lg:my-24 w-full max-w-sm mx-auto"
+            src="/images/nest-head.png"
+          />
+        </kinesis-element>
+
+        <img
+          class="-mt-40 mb-16 lg:mb-24 w-full max-w-sm mx-auto"
+          src="/images/nest-body.png"
         />
       </div>
-    </div>
 
-    <Button data-aos="fade-up" class="mt-12">Kaufen</Button>
-  </section>
+      <!-- Features -->
+      <div
+        class="grid grid-cols-3 gap-y-12 lg:gap-x-6 max-w-5xl mx-auto"
+      >
+        <div
+          v-for="(item, i) in items"
+          :key="item.label"
+          data-aos="fade-up"
+          :data-aos-delay="100 * i"
+          class="col-span-3 lg:col-span-1 flex flex-col items-center space-y-4"
+        >
+          <img class="w-16 h-16" :src="item.src" />
+          <p
+            class="font-black text-center"
+            v-text="item.label"
+          />
+        </div>
+      </div>
+
+      <Button data-aos="fade-up" class="mt-12"
+        >Kaufen</Button
+      >
+    </section>
+  </kinesis-container>
 </template>
 
 <script>
+  import {
+    KinesisContainer,
+    KinesisElement,
+  } from 'vue-kinesis';
+
   export default {
+    components: {
+      KinesisContainer,
+      KinesisElement,
+    },
+
     data() {
       return {
         items: [
