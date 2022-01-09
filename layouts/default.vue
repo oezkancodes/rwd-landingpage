f3c397f48f0e36d0dfc7f4e2368d65aa60c26b40
Özkan Yanikbas <oezkanyanikbas@gmail.com>
2022-01-09 13:22:49 +0100

    Update AOS Config

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,3 +4,19 @@
     <Nuxt />
   </main>
 </template>
+
+<script>
+  import AOS from 'aos';
+  import 'aos/dist/aos.css';
+
+  export default {
+    mounted() {
+      AOS.init();
+      document
+        .querySelectorAll('img')
+        .forEach((img) =>
+          img.addEventListener('load', () => AOS.refresh())
+        );
+    },
+  };
+</script>
