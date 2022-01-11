270f03b34e7337f63956facc5399ea348231bcd5
Özkan Yanikbas <oezkanyanikbas@gmail.com>
2022-01-11 10:09:54 +0100

    Add Buttons States & Styles

--- components/Button.vue
+++ components/Button.vue
@@ -1,10 +1,10 @@
 <template>
   <button
-    class="rounded-l-full rounded-r-full py-2 lg:py-3 px-6 lg:px-8 uppercase"
+    class="rounded-l-full rounded-r-full py-2 lg:py-3 px-6 lg:px-8 uppercase transition duration-200"
     :class="
       white
-        ? 'text-black bg-white'
-        : 'text-white bg-red-600'
+        ? 'text-black bg-white border-2 border-gray-300 hover:bg-gray-100 active:bg-gray-200'
+        : 'text-white bg-red-600 hover:bg-red-700 active:bg-red-800'
     "
   >
     <slot name="default" />
