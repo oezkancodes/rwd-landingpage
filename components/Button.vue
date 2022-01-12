83f14557fcc663a591065d7291f1560d856f7ca8
Özkan Yanikbas <oezkanyanikbas@gmail.com>
2022-01-12 18:08:38 +0100

    Add gray Button variant

--- components/Button.vue
+++ components/Button.vue
@@ -1,11 +1,20 @@
 <template>
   <button
     class="rounded-l-full rounded-r-full py-2 lg:py-3 px-6 lg:px-8 uppercase transition duration-200"
-    :class="
-      white
-        ? 'text-black bg-white border-2 border-gray-300 hover:bg-gray-100 active:bg-gray-200'
-        : 'text-white bg-red-600 hover:bg-red-700 active:bg-red-800'
-    "
+    :class="[
+      {
+        'text-white bg-red-600 hover:bg-red-700 active:bg-red-800':
+          !white && !gray,
+      },
+      {
+        'text-black bg-white border-2 border-gray-300 hover:bg-gray-100 active:bg-gray-200':
+          white && !gray,
+      },
+      {
+        'text-white bg-gray-500 hover:bg-gray-600 active:bg-gray-700':
+          gray && !white,
+      },
+    ]"
   >
     <slot name="default" />
   </button>
@@ -15,6 +24,10 @@
   export default {
     props: {
       white: {
+        type: Boolean,
+        default: false,
+      },
+      gray: {
         type: Boolean,
         default: false,
       },
